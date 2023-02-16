bcd4981b0a05267ac9ede4a42db5da4f015dc0e3
vuillonvalentin <81644808+vuillonvalentin@users.noreply.github.com>
2023-02-16 20:20:57 +0100

    Update wavetable creator.py
    Traduction  en anglais

--- wavetable creator.py
+++ wavetable creator.py
@@ -31,10 +31,10 @@ def fs5(x,y,w):
 
 #complex functions (functions defined with other functions)
 def fc1(x,y):
-    return signe(fs1(x,y,10))
+    return sign(fs1(x,y,10))
 
 def fc2(x,y,w):
-    return signe(fs3(x, y, w))
+    return sign(fs3(x, y, w))
     
 
 def fc3(x,y):
@@ -50,16 +50,16 @@ def fc6(x,y):
     return fc5(x,y)*fs4(x,y,2)
 
 def fc7(x,y):
-    return demultiplication_translation_somme_2args(5, 0.7, x, y, fs4, 10)
+    return demultiplication_translation_sum_2args(5, 0.7, x, y, fs4, 10)
 
 def fc8(x,y):
-    return demultiplication_translation_somme_4args(5, 0.7, x, y, fc4, 10, 4)
+    return demultiplication_translation_sum_4args(5, 0.7, x, y, fc4, 10, 4)
 
 def fc9(x,y):
-    return demultiplication_translation_somme_2args(5, 0.7, x, y, fc5)
+    return demultiplication_translation_sum_2args(5, 0.7, x, y, fc5)
 
 def fc10(x,y):
-    return demultiplication_translation_somme_3args(5, 0.7,x, y, fs5, 20)
+    return demultiplication_translation_sum_3args(5, 0.7,x, y, fs5, 20)
 
 def fc11(x,y):
     x_warp_3args(x, y, fs5,20)
@@ -79,11 +79,11 @@ def fc14(x,y):
 
 #simple operators
 
-#take a function and returns its sign
-def signe(arr):  
+#takes a function and returns its sign
+def sign(arr):  
     arr1=arr
     for i in range(waveform_number):
-        for j in range(sample_number_x):
+        for j in range(sample_number):
             if arr1[i][j]>0:
                 arr1[i][j]=1
             if arr1[i][j]<0:
@@ -91,9 +91,9 @@ def signe(arr):
     return arr1
 
 
-#take a function and demultpliy it. N: number of copies. d: définines distance between copies.
-def demultiplication_translation_somme_2args(N,d, x,y, f):     
-    arr1=np.zeros([waveform_number, sample_number_x])
+#takes a function and demultiplies it. N: number of copies. d: defines the distance between copies
+def demultiplication_translation_sum_2args(N,d, x,y, f):     
+    arr1=np.zeros([waveform_number, sample_number])
     for a in np.linspace(-d,d,N):
         arr1+=f(x+a,y+a)
     if N%2==0:
@@ -105,8 +105,8 @@ def demultiplication_translation_somme_2args(N,d, x,y, f):
       
     return arr1/4
 
-def demultiplication_translation_somme_3args(N,d,x,y, f, extra_parameter1):
-    arr1=np.zeros([waveform_number, sample_number_x])
+def demultiplication_translation_sum_3args(N,d,x,y, f, extra_parameter1):
+    arr1=np.zeros([waveform_number, sample_number])
     for a in np.linspace(-d,d,N):
         arr1+=f(x+a,y+a,extra_parameter1)
     if N%2==0:
@@ -118,8 +118,8 @@ def demultiplication_translation_somme_3args(N,d,x,y, f, extra_parameter1):
       
     return arr1/4
 
-def demultiplication_translation_somme_4args(N,d,x,y, f, extra_parameter1, extra_parameter2):
-    arr1=np.zeros([waveform_number, sample_number_x])
+def demultiplication_translation_sum_4args(N,d,x,y, f, extra_parameter1, extra_parameter2):
+    arr1=np.zeros([waveform_number, sample_number])
     for a in np.linspace(-d,d,N):
         arr1+=f(x+a,y+a,extra_parameter1,extra_parameter2)
     if N%2==0:
@@ -131,11 +131,11 @@ def demultiplication_translation_somme_4args(N,d,x,y, f, extra_parameter1, extra
       
     return arr1/4
 
-#an operator which acts on x and y
+#an operator which acts on x 
 def x_warp_3args(x,y,f,extra_parameter1):  
     return f(-x**2,y,extra_parameter1)
 
-#an operator which make the function rotate around -z with an angle theta in radian
+#an operator which make the function rotate around -z with an angle theta [rad]
 def rotation_2args(theta, x,y,f):  
     x_prime=np.cos(theta)*x-np.sin(theta)*y
     y_prime=np.sin(theta)*x+np.cos(theta)*y
@@ -150,6 +150,7 @@ def rotation_3args(theta, x,y,f,extra_parameter1):
 
 #complex operators (operators defined from other operators)
 
+#to be created
 
 #---------------------------------------------------
 
@@ -171,16 +172,16 @@ X, Y = np.meshgrid(x, y)
 Z = fc4(X,Y,10,4)  #change the function name here. This function will become the wavetable
 
 
-#je coupe tout valeur au delà de -1 et 1, c'est ce que fait serum, ableton wavetable
+#I cut any values below -1 and above 1 it's what serum and ableton wavetable does
 for i in range(waveform_number):
-    for j in range(sample_number_x):
+    for j in range(sample_number):
         if Z[i][j]>1:
             Z[i][j]=1
         if Z[i][j]<-1:
             Z[i][j]=-1
             
             
-#visualisation de la wavetable que serum créera
+#visualizing the wavetable
 if prototyping==True:
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -189,14 +190,14 @@ if prototyping==True:
 print(Z.shape)
 
 
-#je cree le fichier .wav
-wav=np.ones(sample_number_x*waveform_number, dtype='float32') #dtype='float32' permet d'avoir des fichiers lisibles par ableton, ableton wavetable mais ça ne sert pas le cas de serum,
-#ne pas mettre la commande crée des fichiers 64 bit que serum arrive à lire
+#.wav file is created
+wav=np.ones(sample_number*waveform_number, dtype='float32') #dtype='float32' is import for ableton to read the files.
+#but serum is ok with reading 64 bit files so it's not necessary to use the command for serum
 
 for i in range(waveform_number):
-    for j in range(sample_number_x):
-        wav[j+i*sample_number_x]=Z[i][j]
+    for j in range(sample_number):
+        wav[j+i*sample_number]=Z[i][j]
 
-rate = 44100 # c'est sans importance
+rate = 44100 # any number can be used
 if prototyping==False:
     write('wavetable.wav', rate, wav)
